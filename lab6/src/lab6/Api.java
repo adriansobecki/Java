e96cf8489e6c61f595b2ee30e15da386da00b81d
rafal166 <27373088+rafal166@users.noreply.github.com>
2021-05-19 15:01:43 +0200

    cos nie dziala

--- lab6/src/lab6/Api.java
+++ lab6/src/lab6/Api.java
@@ -6,7 +6,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 //import com.google.gson.Gson;
 
 public class Api {
