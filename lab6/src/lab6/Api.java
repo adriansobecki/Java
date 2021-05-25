5f18f2bb01e97e06e9da88122a8d11fc8a8203fd
Adrian Sobecki <adriansobecki@onet.pl>
2021-05-25 19:06:22 +0200

    dzialajace api

--- lab6/src/lab6/Api.java
+++ lab6/src/lab6/Api.java
@@ -6,11 +6,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import com.google.gson.Gson;
+
 //import com.google.gson.Gson;
 //import com.google.gson.Gson;
 
 public class Api {
-	public static void get(String city) throws Exception {
+	public static Weather get(String city) throws Exception {
 	    HttpClient client = HttpClient.newHttpClient();
 	    HttpRequest request = HttpRequest.newBuilder()
 	          .uri(URI.create("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=08ba5dd31d0f7c44acd5ee3c8d364099"))
@@ -22,9 +24,13 @@ public class Api {
 	    System.out.println(response.body());
 	    String json=response.body().toString();
 
-        //Weather data = new Gson().fromJson(json, Weather.class);
+        Weather data = new Gson().fromJson(json, Weather.class);
 
         // Show it.
-        //System.out.println(data);
+        System.out.println(data);
+        return data;
+        //System.out.println(data.weather.get(0).id);
+        //System.out.println(data.name);
+        
 	}
 }
--- lab6/src/lab6/Weather.java
+++ lab6/src/lab6/Weather.java
@@ -1,43 +1,44 @@
 package lab6;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Weather
 {
     public int visibility;
-    //public Main main;
-    //public List<weatherDescription> weather;
+    public Main main;
+    public ArrayList<weatherDescription> weather;
     public String name;
 
     public class Main
     {
-        private double temp;
-        private double temp_min;
-        private double temp_max;
-        private double pressure;
+    	public double temp;
+    	public double temp_min;
+    	public double temp_max;
+    	public double pressure;
 
     }
 
     public class weatherDescription
     {
-    	private String id;
-    	private String main;
-    	private String description;
-    	private String icon;
+    	public String id;
+    	public String main;
+    	public String description;
+    	public String icon;
     }
     
     public int getvisibility() { return visibility; }
-    //public Main getmain() { return main; }
-    //public List<weatherDescription> getweather() { return weather; }
+    public Main getmain() { return main; }
+    public List<weatherDescription> getweather() { return weather; }
     public String getname() { return name; }
 
     public void setvisibility(int visibility) { this.visibility = visibility; }
-    //public void setmain(Main main) { this.main = main; }
-    //public void setweather(List<weatherDescription> weather) { this.weather = weather; }
+    public void setmain(Main main) { this.main = main; }
+    public void setweather(ArrayList<weatherDescription> weather) { this.weather = weather; }
     public void setname(String name) { this.name = name; }
 
     public String toString() {
-        return String.format("title:%s,id:%d", visibility,name);
+        return String.format("visibility:%d,name:%s,temp:%f,temp_min:%f,temp_max:%f,pressure:%f,id:%s,main:%s,description:%s,icon:%s", visibility,name,main.temp,main.temp_min,main.temp_max,main.pressure,weather.get(0).id,weather.get(0).main,weather.get(0).description,weather.get(0).icon);
     }
 }
 
